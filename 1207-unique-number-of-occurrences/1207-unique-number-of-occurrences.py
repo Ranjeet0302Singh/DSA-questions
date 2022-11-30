21d1fecec47efe52af21e873cb9f7bead3815605
Ranjeet Singh <93567891+Ranjeet0302Singh@users.noreply.github.com>
2022-11-30 21:47:35 +0530

    Time: 51 ms (77.32%), Space: 14.1 MB (5.18%) - LeetHub

--- 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.py
+++ 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.py
@@ -1,19 +1,12 @@
 class Solution:
     def uniqueOccurrences(self, arr: List[int]) -> bool:
-        myDict={}
-        for i in arr:
-            if i not in myDict:
-                myDict[i]=1
-            else:
-                myDict[i]+=1
-        print(myDict)
-        ans=set()
-        arr=sorted(list(set(arr)))
-        for i in range(len(myDict)):
-            print(myDict[arr[i]])
-            if myDict[arr[i]] not in ans:
-                ans.add(myDict[arr[i]])
-            else:
-                return False
-        return True
-        
+        arr=Counter(arr)
+        print(arr)
+        arrValue=set(Counter(arr).values())
+        arrkey=set(Counter(arr).keys())
+        print(arrValue)
+        print(arrkey)
+        if len(arrkey) == len(arrValue):
+            return True
+        else:
+            return False
